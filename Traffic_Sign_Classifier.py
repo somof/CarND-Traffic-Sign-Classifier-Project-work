8767fa2f391b63743570ada3bb8322367d672428
somof <gituser@somof.net>
2017-06-05 21:23:13 +0900

    the first imple. of LeNet

--- Traffic_Sign_Classifier.py
+++ Traffic_Sign_Classifier.py
@@ -1,5 +1,9 @@
+# Preprocess Data
+from sklearn.utils import shuffle
 
-import matplotlib.pyplot as plt
+# Setup TensorFlow
+import tensorflow as tf
+from tensorflow.contrib.layers import flatten
 
 
 # Step 0: Load The Data
@@ -7,11 +11,9 @@ import matplotlib.pyplot as plt
 # Load pickled data
 import pickle
 
-# TODO: Fill this in based on where you saved the training and testing data
-
-training_file = 'traffic-signs-data/train.p'
-validation_file= 'traffic-signs-data/valid.p'
-testing_file = 'traffic-signs-data/test.p'
+training_file   = 'traffic-signs-data/train.p'
+validation_file = 'traffic-signs-data/valid.p'
+testing_file    = 'traffic-signs-data/test.p'
 
 with open(training_file, mode='rb') as f:
     train = pickle.load(f)
@@ -19,7 +21,7 @@ with open(validation_file, mode='rb') as f:
     valid = pickle.load(f)
 with open(testing_file, mode='rb') as f:
     test = pickle.load(f)
-    
+
 X_train, y_train = train['features'], train['labels']
 X_valid, y_valid = valid['features'], valid['labels']
 X_test, y_test = test['features'], test['labels']
@@ -38,23 +40,23 @@ X_test, y_test = test['features'], test['labels']
 # 'sizes'    : リスト (width, height) original size
 # 'coords'   : タプルのリスト [(x1, y1, x2, y2), ...]  bounding box の位置
 
-### Replace each question mark with the appropriate value. 
+### Replace each question mark with the appropriate value.
 ### Use python, pandas or numpy methods rather than hard coding the results
 
 
-# TODO: Number of training examples
+# Number of training examples
 n_train = len(X_train)
 
-# TODO: Number of validation examples
+# Number of validation examples
 n_validation = len(y_valid)
 
-# TODO: Number of testing examples.
+# Number of testing examples.
 n_test = len(y_test)
 
-# TODO: What's the shape of an traffic sign image?
+# What's the shape of an traffic sign image?
 image_shape = X_train[0].shape
 
-# TODO: How many unique classes/labels there are in the dataset.
+# How many unique classes/labels there are in the dataset.
 n_classes = len(set(y_train))
 
 print("Number of training examples =", n_train)
@@ -106,8 +108,6 @@ print("Number of classes =", n_classes)
 ### preprocessing steps could include converting to grayscale, etc.
 ### Feel free to use as many code cells as needed.
 
-from sklearn.utils import shuffle
-
 X_train, y_train = shuffle(X_train, y_train)
 
 
@@ -116,9 +116,6 @@ X_train, y_train = shuffle(X_train, y_train)
 ### Define your architecture here.
 ### Feel free to use as many code cells as needed.
 
-import tensorflow as tf
-from tensorflow.contrib.layers import flatten
-
 EPOCHS = 10
 BATCH_SIZE = 128
 FILTER1_NUM = 24  # 6
@@ -126,14 +123,15 @@ FILTER2_NUM = 128  # 16
 FRC3_NUM = 400  # 120
 FRC4_NUM = 200  # 84
 
-def LeNet(x):    
+
+def LeNet(x):
     # Arguments used for tf.truncated_normal, randomly defines variables for
     # the weights and biases for each layer
     mu = 0
     sigma = 0.1
-    
+
     # Layer 1: Convolutional. Input = 32x32x1. Output = 28x28xFILTER1_NUM.
-    conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 3, FILTER1_NUM), mean = mu, stddev = sigma))
+    conv1_W = tf.Variable(tf.truncated_normal(shape=(5, 5, 3, FILTER1_NUM), mean=mu, stddev=sigma))
     conv1_b = tf.Variable(tf.zeros(FILTER1_NUM))
     conv1   = tf.nn.conv2d(x, conv1_W, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
 
@@ -144,10 +142,10 @@ def LeNet(x):
     conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
 
     # Layer 2: Convolutional. Output = 10x10xFILTER2_NUM.
-    conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, FILTER1_NUM, FILTER2_NUM), mean = mu, stddev = sigma))
+    conv2_W = tf.Variable(tf.truncated_normal(shape=(5, 5, FILTER1_NUM, FILTER2_NUM), mean=mu, stddev=sigma))
     conv2_b = tf.Variable(tf.zeros(FILTER2_NUM))
     conv2   = tf.nn.conv2d(conv1, conv2_W, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
-    
+
     # Activation.
     conv2 = tf.nn.relu(conv2)
 
@@ -156,28 +154,28 @@ def LeNet(x):
 
     # Flatten. Input = 5x5x16. Output = 400.
     fc0   = flatten(conv2)
-    
+
     # Layer 3: Fully Connected. Input = 400. Output = FRC3_NUM.
-    fc1_W = tf.Variable(tf.truncated_normal(shape=(25*FILTER2_NUM, FRC3_NUM), mean = mu, stddev = sigma))
+    fc1_W = tf.Variable(tf.truncated_normal(shape=(25 * FILTER2_NUM, FRC3_NUM), mean=mu, stddev=sigma))
     fc1_b = tf.Variable(tf.zeros(FRC3_NUM))
     fc1   = tf.matmul(fc0, fc1_W) + fc1_b
-    
+
     # Activation.
     fc1    = tf.nn.relu(fc1)
 
     # Layer 4: Fully Connected. Input = FRC3_NUM. Output = FRC4_NUM.
-    fc2_W  = tf.Variable(tf.truncated_normal(shape=(FRC3_NUM, FRC4_NUM), mean = mu, stddev = sigma))
+    fc2_W  = tf.Variable(tf.truncated_normal(shape=(FRC3_NUM, FRC4_NUM), mean=mu, stddev=sigma))
     fc2_b  = tf.Variable(tf.zeros(FRC4_NUM))
     fc2    = tf.matmul(fc1, fc2_W) + fc2_b
-    
+
     # Activation.
     fc2    = tf.nn.relu(fc2)
 
     # Layer 5: Fully Connected. Input = FRC4_NUM. Output = 43.
-    fc3_W  = tf.Variable(tf.truncated_normal(shape=(FRC4_NUM, 43), mean = mu, stddev = sigma))
+    fc3_W  = tf.Variable(tf.truncated_normal(shape=(FRC4_NUM, 43), mean=mu, stddev=sigma))
     fc3_b  = tf.Variable(tf.zeros(43))
     logits = tf.matmul(fc2, fc3_W) + fc3_b
-    
+
     return logits
 
 
@@ -201,7 +199,7 @@ rate = 0.001
 logits = LeNet(x)
 cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y, logits=logits)
 loss_operation = tf.reduce_mean(cross_entropy)
-optimizer = tf.train.AdamOptimizer(learning_rate = rate)
+optimizer = tf.train.AdamOptimizer(learning_rate=rate)
 training_operation = optimizer.minimize(loss_operation)
 
 
@@ -214,24 +212,23 @@ correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
 accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 saver = tf.train.Saver()
 
+
 def evaluate(X_data, y_data):
     num_examples = len(X_data)
     total_accuracy = 0
     sess = tf.get_default_session()
     for offset in range(0, num_examples, BATCH_SIZE):
-        batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
+        batch_x, batch_y = X_data[offset:offset + BATCH_SIZE], y_data[offset:offset + BATCH_SIZE]
         accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
         total_accuracy += (accuracy * len(batch_x))
     return total_accuracy / num_examples
-
-
 
 
 # ## Train the Model
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
     num_examples = len(X_train)
-    
+
     print("Training...")
     print()
     for i in range(EPOCHS):
@@ -240,12 +237,12 @@ with tf.Session() as sess:
             end = offset + BATCH_SIZE
             batch_x, batch_y = X_train[offset:end], y_train[offset:end]
             sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
-            
+
         validation_accuracy = evaluate(X_valid, y_valid)
-        print("EPOCH {} ...".format(i+1))
+        print("EPOCH {} ...".format(i + 1))
         print("Validation Accuracy = {:.3f}".format(validation_accuracy))
         print()
-        
+
     saver.save(sess, './lenet')
     print("Model saved")
 
@@ -257,4 +254,3 @@ with tf.Session() as sess:
 
     test_accuracy = evaluate(X_test, y_test)
     print("Test Accuracy = {:.3f}".format(test_accuracy))
-

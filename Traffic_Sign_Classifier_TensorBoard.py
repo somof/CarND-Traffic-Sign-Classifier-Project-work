0c42cfe4654fea5a699144ca72c4c9ff19974ad6
Hiroshi Ichiki <Hiroshi.Ichiki@sony.com>
2017-06-13 20:28:49 +0900

    fix TensorBoard code

--- Traffic_Sign_Classifier_TensorBoard.py
+++ Traffic_Sign_Classifier_TensorBoard.py
@@ -4,20 +4,6 @@ from sklearn.utils import shuffle
 # Setup TensorFlow
 import tensorflow as tf
 from tensorflow.contrib.layers import flatten
-# import tensorboard as tb
-
-
-# Summary functions have been consolidated under the tf.summary namespace.
-# tf.audio_summary should be renamed to tf.summary.audio
-# tf.contrib.deprecated.histogram_summary should be renamed to tf.summary.histogram
-# tf.contrib.deprecated.scalar_summary should be renamed to tf.summary.scalar
-# tf.histogram_summary should be renamed to tf.summary.histogram
-# tf.image_summary should be renamed to tf.summary.image
-# tf.merge_all_summaries should be renamed to tf.summary.merge_all
-# tf.merge_summary should be renamed to tf.summary.merge
-# tf.scalar_summary should be renamed to tf.summary.scalar
-# tf.train.SummaryWriter should be renamed to tf.summary.FileWriter
-
 
 
 
@@ -74,8 +60,9 @@ image_shape = X_train[0].shape
 # How many unique classes/labels there are in the dataset.
 n_classes = len(set(y_train))
 
-print("Number of training examples =", n_train)
-print("Number of testing examples =", n_test)
+print("Number of training examples    =", n_train)
+print("Number of validation examples  =", n_validation)
+print("Number of testing examples     =", n_test)
 print("Image data shape =", image_shape)
 print("Number of classes =", n_classes)
 
@@ -111,7 +98,7 @@ print("Number of classes =", n_classes)
 ### preprocessing steps could include converting to grayscale, etc.
 ### Feel free to use as many code cells as needed.
 
-X_train, y_train = shuffle(X_train, y_train)
+# X_train, y_train = shuffle(X_train, y_train)
 
 
 
@@ -119,90 +106,99 @@ X_train, y_train = shuffle(X_train, y_train)
 ### Define your architecture here.
 ### Feel free to use as many code cells as needed.
 
-EPOCHS      = 4
-BATCH_SIZE  = 100
-FILTER1_NUM =   6  #  10  #   6
-FILTER2_NUM =  12  #  20  #  16
-FRC1_NUM    =  64  # 100  # 120
-FRC2_NUM    =  32  #  60  #  84
-CLASS_NUM   = 43
+EPOCHS      =  20
+BATCH_SIZE  = 400  # 32  #  64
+FILTER1_NUM =  16  #  6  #  10  #   6
+FILTER2_NUM =  32  # 12  #  20  #  16
+FRC1_NUM    = 128  # 64  # 100  # 120
+FRC2_NUM    =  80  # 32  #  60  #  84
+CLASS_NUM   =  43
 MU          = 0
 SIGMA       = 0.1
-
-
-# Arguments used for tf.truncated_normal, randomly defines variables for
-# the weights and biases for each layer
-weights = {
-    'wc1': tf.Variable(tf.truncated_normal(shape=(5, 5, 3, FILTER1_NUM), mean=MU, stddev=SIGMA)),
-    'wc2': tf.Variable(tf.truncated_normal(shape=(5, 5, FILTER1_NUM, FILTER2_NUM), mean=MU, stddev=SIGMA)),
-    'wf1': tf.Variable(tf.truncated_normal(shape=(5 * 5 * FILTER2_NUM, FRC1_NUM), mean=MU, stddev=SIGMA)),
-    'wf2': tf.Variable(tf.truncated_normal(shape=(FRC1_NUM, FRC2_NUM), mean=MU, stddev=SIGMA)),
-    'wf3': tf.Variable(tf.truncated_normal(shape=(FRC2_NUM, CLASS_NUM), mean=MU, stddev=SIGMA))}
-
-biases = {
-    'bc1': tf.Variable(tf.truncated_normal(shape=(FILTER1_NUM,), mean=MU, stddev=SIGMA)),
-    'bc2': tf.Variable(tf.truncated_normal(shape=(FILTER2_NUM,), mean=MU, stddev=SIGMA)),
-    'bf1': tf.Variable(tf.truncated_normal(shape=(FRC1_NUM,), mean=MU, stddev=SIGMA)),
-    'bf2': tf.Variable(tf.truncated_normal(shape=(FRC2_NUM,), mean=MU, stddev=SIGMA)),
-    'bf3': tf.Variable(tf.truncated_normal(shape=(CLASS_NUM,), mean=MU, stddev=SIGMA))}
 
 
 def LeNet(x):
 
     with tf.name_scope('conv1'):
         # Layer 1: Convolutional. Input = 32x32x1. Output = 28x28xFILTER1_NUM.
-        conv1 = tf.nn.conv2d(x, weights['wc1'], strides=[1, 1, 1, 1], padding='VALID') + biases['bc1']
+        conv1_w = tf.Variable(tf.truncated_normal(shape=(5, 5, 3, FILTER1_NUM), mean=MU, stddev=SIGMA))
+        conv1_b = tf.Variable(tf.truncated_normal(shape=(FILTER1_NUM,), mean=MU, stddev=SIGMA))
+        conv1 = tf.nn.conv2d(x, conv1_w, strides=[1, 1, 1, 1], padding='VALID') + conv1_b
         conv1 = tf.nn.relu(conv1)
         # Pooling. Input = 28x28xFILTER1_NUM. Output = 14x14xFILTER1_NUM.
         conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+        # Tensorboard
+        conv1_w_hist = tf.summary.histogram("conv1_w", conv1_w)
+        conv1_b_hist = tf.summary.histogram("conv1_b", conv1_b)
 
     with tf.name_scope('conv2'):
         # Layer 2: Convolutional. put = 14x14xFILTER1_NUM. Output = 10x10xFILTER2_NUM.
-        conv2 = tf.nn.conv2d(conv1, weights['wc2'], strides=[1, 1, 1, 1], padding='VALID') + biases['bc2']
+        conv2_w = tf.Variable(tf.truncated_normal(shape=(5, 5, FILTER1_NUM, FILTER2_NUM), mean=MU, stddev=SIGMA))
+        conv2_b = tf.Variable(tf.truncated_normal(shape=(FILTER2_NUM,), mean=MU, stddev=SIGMA))
+        conv2 = tf.nn.conv2d(conv1, conv2_w, strides=[1, 1, 1, 1], padding='VALID') + conv2_b
         conv2 = tf.nn.relu(conv2)
         # Pooling. Input = 10x10xFILTER2_NUM. Output = 5x5xFILTER2_NUM.
         conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='VALID')
+        # Tensorboard
+        conv2_w_hist = tf.summary.histogram("conv2_w", conv2_w)
+        conv2_b_hist = tf.summary.histogram("conv2_b", conv2_b)
 
     # Flatten. Input = 5x5xFILTER2_NUM. Output = 5x5xFILTER2_NUM.
     fc0   = flatten(conv2)
 
     with tf.name_scope('fc1'):
         # Layer 3: Fully Connected. Input = 5x5xFILTER2_NUM. Output = FRC1_NUM.
-        fc1   = tf.matmul(fc0, weights['wf1']) + biases['bf1']
+        fc1_w = tf.Variable(tf.truncated_normal(shape=(5 * 5 * FILTER2_NUM, FRC1_NUM), mean=MU, stddev=SIGMA))
+        fc1_b = tf.Variable(tf.truncated_normal(shape=(FRC1_NUM,), mean=MU, stddev=SIGMA))
+        fc1   = tf.matmul(fc0, fc1_w) + fc1_b
         fc1   = tf.nn.relu(fc1)
         # fc1   = tf.nn.dropout(fc1, 0.5)
+        # Tensorboard
+        fc1_w_hist = tf.summary.histogram("fc1_w", fc1_w)
+        fc1_b_hist = tf.summary.histogram("fc1_b", fc1_b)
 
     with tf.name_scope('fc2'):
         # Layer 4: Fully Connected. Input = FRC1_NUM. Output = FRC2_NUM.
-        fc2   = tf.matmul(fc1, weights['wf2']) + biases['bf2']
+        fc2_w = tf.Variable(tf.truncated_normal(shape=(FRC1_NUM, FRC2_NUM), mean=MU, stddev=SIGMA))
+        fc2_b = tf.Variable(tf.truncated_normal(shape=(FRC2_NUM,), mean=MU, stddev=SIGMA))
+        fc2   = tf.matmul(fc1, fc2_w) + fc2_b
         fc2   = tf.nn.relu(fc2)
         # fc2   = tf.nn.dropout(fc2, 0.5)
+        # Tensorboard
+        fc2_w_hist = tf.summary.histogram("fc2_w", fc2_w)
+        fc2_b_hist = tf.summary.histogram("fc2_b", fc2_b)
 
-    # Layer 5: Fully Connected. Input = FRC2_NUM. Output = CLASS_NUM.
-    logits = tf.matmul(fc2, weights['wf3']) + biases['bf3']
+    with tf.name_scope('output'):
+        # Layer 5: Fully Connected. Input = FRC2_NUM. Output = CLASS_NUM.
+        fc3_w  = tf.Variable(tf.truncated_normal(shape=(FRC2_NUM, CLASS_NUM), mean=MU, stddev=SIGMA))
+        fc3_b  = tf.Variable(tf.truncated_normal(shape=(CLASS_NUM,), mean=MU, stddev=SIGMA))
+        logits = tf.matmul(fc2, fc3_w) + fc3_b
+        # Tensorboard
+        fc3_w_hist = tf.summary.histogram("fc3_w", fc3_w)
+        fc3_b_hist = tf.summary.histogram("fc3_b", fc3_b)
 
     return logits
 
 
-# with tf.name_scope('dropout'):
-#   keep_prob = tf.placeholder(tf.float32)
-#   tf.summary.scalar('dropout_keep_probability', keep_prob)
-#   dropped = tf.nn.dropout(hidden1, keep_prob)
+# input & output unit
+with tf.name_scope('input'):
+    x = tf.placeholder(tf.float32, (None, 32, 32, 3))
 
-
-x = tf.placeholder(tf.float32, (None, 32, 32, 3))
-y = tf.placeholder(tf.int32, (None))
-one_hot_y = tf.one_hot(y, CLASS_NUM)
-
+with tf.name_scope('one_hot'):
+    y = tf.placeholder(tf.int32, (None))
+    one_hot_y = tf.one_hot(y, CLASS_NUM)
 logits = LeNet(x)
 
-with tf.name_scope('cross_entropy'):
-    cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y, logits=logits)
-tf.summary.scalar('cross_entropy', cross_entropy)
-
 with tf.name_scope('loss'):
-    loss_operation = tf.reduce_mean(cross_entropy)
-tf.summary.scalar('loss', loss_operation)
+    with tf.name_scope('cross_entropy'):
+        cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y, logits=logits)
+    with tf.name_scope('loss_operation'):
+        loss_operation = tf.reduce_mean(cross_entropy)
+
+
+
+
+
 
 
 
@@ -213,154 +209,122 @@ tf.summary.scalar('loss', loss_operation)
 ### Feel free to use as many code cells as needed.
 
 rate = 0.0001  # Slow to train
-rate = 0.005   # with Dropout X
-rate = 0.0005  # without Dropout
 rate = 0.001   # @ pre learning
+rate = 0.0007  #
 
 with tf.name_scope('train'):
     optimizer = tf.train.AdamOptimizer(learning_rate=rate)
     training_operation = optimizer.minimize(loss_operation)
-
-
-
-
-
-# ## Model Evaluation
-
-def evaluate(X_data, y_data):
-    num_examples = len(X_data)
-    total_accuracy = 0
-    sess = tf.get_default_session()
-    for offset in range(0, num_examples, BATCH_SIZE):
-        batch_x, batch_y = X_data[offset:offset + BATCH_SIZE], y_data[offset:offset + BATCH_SIZE]
-        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
-        total_accuracy += (accuracy * len(batch_x))
-    return total_accuracy / num_examples
-
-
-# correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
-# accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 with tf.name_scope('accuracy'):
     with tf.name_scope('correct_prediction'):
         correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
     with tf.name_scope('accuracy'):
         accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-tf.summary.scalar('accuracy', accuracy_operation)
-# tf.summary.scalar("valid accuracy", evaluate(X_valid, y_valid))
-# tf.summary.scalar("test accuracy", evaluate(X_test, y_test))
 
 
 
 # ## Train the Model
-saver = tf.train.Saver()
+# saver = tf.train.Saver()
 last_validation_accuracy = 0.9515
-last_validation_accuracy = 0.55
+
 with tf.Session() as sess:
 
     # Tensorboard logdir
-    if tf.gfile.Exists('log'):
-        tf.gfile.DeleteRecursively('log')
-    tf.gfile.MakeDirs('log')
-
-    # Merge all the summaries and write them out to /tmp/mnist_logs (by default)
+    boarddir = 'log'
+    if tf.gfile.Exists(boarddir):
+        tf.gfile.DeleteRecursively(boarddir)
+    tf.gfile.MakeDirs(boarddir)
+    
+    summary_writer = tf.summary.FileWriter("log", sess.graph)
+    acc_train = tf.summary.scalar('accuracy', accuracy_operation)
+    loss_train = tf.summary.scalar('loss', loss_operation)
+    # acc_valid = tf.summary.scalar('valid accuracy', accuracy_operation)
+    # loss_valid = tf.summary.scalar('valid loss', loss_operation)
     merged = tf.summary.merge_all()
-    # train_writer = tf.summary.FileWriter(FLAGS.summaries_dir + '/train', sess.graph)
-    # test_writer = tf.summary.FileWriter(FLAGS.summaries_dir + '/test')
-    summary_writer = tf.summary.FileWriter("log/", sess.graph)
-    ## tf.global_variables_initializer().run()
 
+
+
+    # Initialize & Train
     sess.run(tf.global_variables_initializer())
     num_examples = len(X_train)
 
-    # Tensorboard
-    # tf.summary.scalar("valid accuracy", evaluate(X_valid, y_valid))
-    # tf.summary.scalar("test accuracy", evaluate(X_test, y_test))
-    # tf.summary.scalar("valid accuracy", validation_accuracy)
-    # tf.summary.scalar("test accuracy", test_accuracy)
-
-    # test code
-    # ckpt = tf.train.get_checkpoint_state('./')
-    # if None and ckpt:  # checkpointがある場合
-    #     last_model = ckpt.model_checkpoint_path  # 最後に保存したmodelへのパス
-    #     print("load " + last_model)
-    #     saver.restore(sess, last_model)  # 変数データの読み込み
-    #     # test code
-    # else:
-    #     print("Training...")
-    #     print()
-    #     last_validation_accuracy = 0.0
-    #     for i in range(EPOCHS):
-    #         X_train, y_train = shuffle(X_train, y_train)
-    #         for offset in range(0, num_examples, BATCH_SIZE):
-    #             end = offset + BATCH_SIZE
-    #             batch_x, batch_y = X_train[offset:end], y_train[offset:end]
-    #             sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
-
-    #         print("EPOCH {} ...".format(i + 1))
-    #         validation_accuracy = evaluate(X_valid, y_valid)
-    #         test_accuracy = evaluate(X_test, y_test)
-    #         print("Validation Accuracy = {:.5f}".format(validation_accuracy))
-    #         print("test Accuracy = {:.5f}".format(test_accuracy))
-    #         if last_validation_accuracy < validation_accuracy:
-    #             last_validation_accuracy = validation_accuracy
-    #             saver.save(sess, './lenet')
-    #             print("Model saved")
-
-    #         # Tensorboard
-    #         print()
-
-    print("Additional Training...")
+    print("Training...")
     print()
     for i in range(EPOCHS):
+        print("\nEPOCH {} ...".format(i + 1))
+
         X_train, y_train = shuffle(X_train, y_train)
         for offset in range(0, num_examples, BATCH_SIZE):
             end = offset + BATCH_SIZE
             batch_x, batch_y = X_train[offset:end], y_train[offset:end]
-            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
 
-        print("EPOCH {} ...".format(i + 1))
-        summary_writer = tf.summary.FileWriter("log/", sess.graph)
+            # Merged summary
+            summary, __  = sess.run([merged, training_operation], feed_dict={x: batch_x, y: batch_y})
 
-        # validation_accuracy = evaluate(X_valid, y_valid)
-        # test_accuracy = evaluate(X_test, y_test)
-        # print("Validation Accuracy = {:.3f}".format(validation_accuracy))
-        # print("test Accuracy = {:.5f}".format(test_accuracy))
-        # if last_validation_accuracy < validation_accuracy:
-        #     last_validation_accuracy = validation_accuracy
-        #     saver.save(sess, './lenet')
-        #     print("Model saved")
+            # Tensorboard each summary
+            # sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
+            # train_list = [accuracy_operation, training_operation, acc_train, loss_train]
+            # result = sess.run(train_list, feed_dict={x: batch_x, y: batch_y})
+            # for j in range(1,len(result)):
+            #     summary_writer.add_summary(result[j], i)
+        
+        # Tensorboard each summary
+        # valid_list = [acc_valid, loss_valid]
+        # result = sess.run(valid_list, feed_dict={x: X_valid, y: y_valid})
+        # for j in range(1,len(result)):
+        #     summary_writer.add_summary(result[j], i)
+
+        validation_accuracy = sess.run(accuracy_operation, feed_dict={x: X_valid, y: y_valid})
+        print("Validation Accuracy = {:.3f}".format(validation_accuracy))
+
+        if last_validation_accuracy < validation_accuracy:
+            last_validation_accuracy = validation_accuracy
+            saver.save(sess, './lenet')
+            print("Model saved")
+            # train_accuracy = sess.run(accuracy_operation, feed_dict={x: X_train, y: y_train})
+            # print("Training Accuracy = {:.3f}".format(train_accuracy))
+            test_accuracy = sess.run(accuracy_operation, feed_dict={x: X_test, y: y_test})
+            print("test Accuracy = {:.5f}".format(test_accuracy))
+            # if 0.957 < validation_accuracy:
+            #     break
 
         # Tensorboard
-        summary, acc = sess.run([merged, accuracy_operation], feed_dict=feed_dict(False))
+        # Merged summary
         summary_writer.add_summary(summary, i)
-        print('Accuracy at step %s: %s' % (i, acc))
+
+    # Closing
+    summary_writer.close()
 
 
-        print()
-        if 0.957 < validation_accuracy:
-            break
+    print()
+    # validation_accuracy = sess.run(valid_list, feed_dict={x: X_valid, y: y_valid})
+    # print("Validation Accuracy = {:.3f}".format(validation_accuracy))
+    test_accuracy = sess.run(accuracy_operation, feed_dict={x: X_test, y: y_test})
+    print("test Accuracy = {:.5f}".format(test_accuracy))
+    print()
+    train_accuracy = sess.run(accuracy_operation, feed_dict={x: X_train, y: y_train})
+    print("Training Accuracy = {:.3f}".format(train_accuracy))
+    print()
 
-    # Visualization
-    # _W = sess.run(logits.conv1_W)
-    # for i in range(10):
-    #     plt.subplot(2, 5, i+1)
-    #     plt.title("W_%d" % i)
-    #     plt.axis("off")
-    #     plt.imshow(_W.transpose()[i].reshape(32, 32), cmap=None)
-    # plt.show()
 
-exit(0)
+# def evaluate(X_data, y_data):
+#     num_examples = len(X_data)
+#     total_accuracy = 0
+#     sess = tf.get_default_session()
+#     for offset in range(0, num_examples, BATCH_SIZE):
+#         batch_x, batch_y = X_data[offset:offset + BATCH_SIZE], y_data[offset:offset + BATCH_SIZE]
+#         accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
+#         total_accuracy += (accuracy * len(batch_x))
+#     return total_accuracy / num_examples
 
-# ## Evaluate the Model
-with tf.Session() as sess:
-    saver.restore(sess, tf.train.latest_checkpoint('.'))
+# Evaluate the Model
+# with tf.Session() as sess:
+#     saver.restore(sess, tf.train.latest_checkpoint('.'))
+#     test_accuracy = evaluate(X_test, y_test)
+#     print("Test Accuracy = {:.3f}".format(test_accuracy))
 
-    test_accuracy = evaluate(X_test, y_test)
-    print("Test Accuracy = {:.3f}".format(test_accuracy))
-
-with tf.Session() as sess:
-    saver.restore(sess, tf.train.latest_checkpoint('.'))
-
-    validation_accuracy = evaluate(X_valid, y_valid)
-    print("Validation Accuracy = {:.3f}".format(validation_accuracy))
+# with tf.Session() as sess:
+#     saver.restore(sess, tf.train.latest_checkpoint('.'))
+#     validation_accuracy = evaluate(X_valid, y_valid)
+#     print("Validation Accuracy = {:.3f}".format(validation_accuracy))
